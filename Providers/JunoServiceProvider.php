b9d7b1bcdf9dd55cef91c73cb84a0273c318c7ca
breakinant <felipesan08@hotmail.com>
2021-11-23 17:41:58 -0300

    Fix in directory

--- Providers/JunoServiceProvider.php
+++ Providers/JunoServiceProvider.php
@@ -23,6 +23,6 @@ class JunoServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'../database/migrations');
     }
 }
